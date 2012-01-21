4e911d097ce87972dd3660fdeb5734829579649b
amitibo <amitibo@localhost>
2012-01-21 17:07:22 +0200

    improved interface, added default values to optimization parameters.

--- test/test_direct.py
+++ test/test_direct.py
@@ -1,4 +1,4 @@
-from DIRECT import direct
+from DIRECT import solve
 import numpy as np
 
 trans = np.array([-1, 2, -4, 3])
@@ -12,36 +12,13 @@ if __name__ == '__main__':
 
     logfile = "direct_log.txt"
 
-    eps = 0.0001
-    maxf = 10000
-    maxT = 100
     l = np.array([-10, -10, -10, -10], dtype=np.float64)
     u = np.array([10, 10, 10, 10], dtype=np.float64)
-    algmethod = 0
-    fglobal = -1000000
-    fglper = 0.0
-    volper = 0.00001
-    sigmaper = 0.001
-    iidata = np.ones(10, dtype=np.int32)
-    ddata = np.ones(10, dtype=np.float64)
-    cdata = np.ones([10,40], dtype=np.uint8)*49
 
-    x, fmin, ierror = direct(
+    x, fmin, ierror = solve(
                         func,
-                        eps,
-                        maxf,
-                        maxT,
                         l,
-                        u,
-                        algmethod,
-                        logfile, 
-                        fglobal,
-                        fglper,
-                        volper,
-                        sigmaper,
-                        iidata,
-                        ddata,
-                        cdata
+                        u
                         )
 
     print x
