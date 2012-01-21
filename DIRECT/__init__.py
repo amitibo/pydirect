ad78aadd6fff052e95d62172566949eaee3ce69a
Amit Aides <664667+amitibo@users.noreply.github.com>
2012-01-22 00:16:45 +0200

    Added documentation. Improved simpler interface. Added the C6, SH optimization problems.
    Note:
    For some reason both C6 and SH don't converage as expected (as in
    the fortran lib documentation)

--- DIRECT/__init__.py
+++ DIRECT/__init__.py
@@ -16,34 +16,149 @@ License: MIT
 import numpy as np
 from direct import direct
 
+ERROR_MESSAGES = (
+    'Maximum number of levels has been reached.',
+    'An error occured while the function was sampled',
+    'There was an error in the creation of the sample points',
+    'Initialization failed',
+    'maxf is too large'
+    'u[i] < l[i] for some i'
+)
+
+SUCCESS_MESSAGES = (
+    'Number of function evaluations done is larger then maxf',
+    'Number of iterations is equal to maxT',
+    'The best function value found is within fglper of the (known) global optimum',
+    'The volume of the hyperrectangle with best function value found is below volper',
+    'The volume of the hyperrectangle with best function value found is smaller then volper'
+)
 
 def solve(
     objective,
     l,
     u,
-    eps=10e-4,
-    maxf=10000,
-    maxT=100,
+    eps=1e-4,
+    maxf=20000,
+    maxT=6000,
     algmethod=0,
-    fglobal=-1000000,
-    fglper=0.0,
-    volper=0.00001,
-    sigmaper=0.001,
+    fglobal=-1e100,
+    fglper=0.01,
+    volper=-1.0,
+    sigmaper=-1.0,
     logfilename='DIRresults.txt',
     user_data=None
     ):
-    
-    iidata = np.ones(10, dtype=np.int32)
-    ddata = np.ones(10, dtype=np.float64)
-    cdata = np.ones([10,40], dtype=np.uint8)*49
+    """
+    Solve an optimization problem using the DIRECT (Dividing Rectangles) algorithm.
+    It can be used to solve general nonlinear programming problems of the form:
 
+            min     f(x)
+            x in R^n
+            
+            s.t.       l <= x <= u
+    
+    Where x are the optimization variables, f(x) is the objective function and
+    l, u are the box constraints.
+
+    Parameters
+    ----------
+    objective : function pointer
+        Callback function for evaluating objective function.
+        The callback functions accepts two parameters: x (value of the
+        optimization variables at which the objective is to be evaluated) and
+        user_data, an arbitrary data object supplied by the user.
+        The function should return a tuple of two values: the objective function
+        value at the point x and a value (flag) that is set to 1 if the function
+        is not defined at point x (0 if it is defined).
+    
+    l : array-like, shape = [n]
+        Lower bounds on variables, where n is the dimension of x.
+        
+    u : array-like, shape = [n]
+        Upper bounds on variables, where n is the dimension of x.
+        
+    eps : float
+        Ensures sufficient decrease in function value when a new potentially
+        optimal interval is chosen (default 1e-4).
+
+    maxf : integer
+        Approximate upper bound on objective function evaluations (default 20000).
+        NOTE:
+        Maximal allowed value is 90000 see documentation of fotran library.
+    
+    maxT : integer
+        Maximum number of iterations (default 6000).
+        NOTE:
+        Maximal allowed value is 6000 see documentation of fotran library.
+        
+    algmethod : integer
+        0 - Use the original DIRECT algorithm
+        1 - Use the modified DIRECT-l algorithm
+    
+    fglobal : float
+        Function value of the global optimum. If this value is not known set this
+        to a very large negative value (default -1e100).
+        
+    fglper : float
+        Terminate the optimization when the percent error satisfies:
+        
+            100*(f_min - f_global)/max(1, |f_global|) < fglper
+        
+        (default 0.01)
+        
+    volper : float
+        Terminate the optimization once the volume of a hyperrectangle is less
+        than volper percent of the original hyperrectangel (default -1.0).
+        
+    sigmaper : float
+        Terminate the optimization once the measure of the hyperrectangle is less
+        than sigmaper (default -1.0)
+        
+    logfilename : string
+        Name of logfile (default 'DIRresults.txt').
+        
+    user_data : object
+        Arbitrary python object used for passing data to the objective function
+        (default None).
+    
+    Returns
+    -------
+    x : array, shape = [n]
+        Final point obtained in the optimization.
+    
+    fmin : float
+        The value of the function at x.
+    
+    ierror : string
+        Status message.
+
+    """
+    
+    def _objective_wrap(x, iidata, ddata, cdata, n, iisize, idsize, icsize):
+        """
+        To simplify the python objective we use a wrapper objective that complies
+        with the required fortran objective.
+        """
+        return objective(x, user_data)
+        
+    #
+    # Dummy values so that the python wrapper will comply with the required
+    # signature of the fortran library.
+    #
+    iidata = np.ones(0, dtype=np.int32)
+    ddata = np.ones(0, dtype=np.float64)
+    cdata = np.ones([0,40], dtype=np.uint8)
+
+    #
+    # Call the DIRECT algorithm
+    #
     x, fmin, ierror = direct(
-                        objective,
+                        _objective_wrap,
                         eps,
                         maxf,
                         maxT,
-                        l,
-                        u,
+                        np.array(l, dtype=np.float64),
+                        np.array(u, dtype=np.float64),
                         algmethod,
                         logfilename, 
                         fglobal,
@@ -55,4 +170,7 @@ def solve(
                         cdata
                         )
 
-    return x, fmin, ierror
+    if ierror < 0:
+        raise Exception(ERROR_MESSAGES[abs(ierror)-1])
+        
+    return x, fmin, SUCCESS_MESSAGES[ierror-1]
